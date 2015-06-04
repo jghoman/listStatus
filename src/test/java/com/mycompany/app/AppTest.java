173b682047d2c2cb38c6ca8df4e6babf20d54b8b
Jakob Homan <jghoman@gmail.com>
2015-06-04 13:59:28 -0700

    Fix Hadoop dependency issue.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,11 +4,9 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.hdfs.web.WebHdfsFileSystem;
 import org.apache.hadoop.hdfs.web.WebHdfsTestUtil;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -16,30 +14,29 @@ import java.net.URISyntaxException;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
   @Test
   public void listStatusReturnsFiles() throws IOException, URISyntaxException {
     MiniDFSCluster cluster = null;
+    FileSystem webHdfs = null;
     final Configuration conf = WebHdfsTestUtil.createConf();
     try {
       cluster = new MiniDFSCluster.Builder(conf).build();
       cluster.waitActive();
-      final DistributedFileSystem dfs = cluster.getFileSystem();
-      final FileSystem webHdfs = WebHdfsTestUtil.getWebHdfsFileSystem(conf,
-          WebHdfsFileSystem.SCHEME);
-      FileSystem fs = webHdfs;
+      webHdfs = WebHdfsTestUtil.getWebHdfsFileSystem(conf, WebHdfsFileSystem.SCHEME);
 
-      fs.mkdirs(new Path("/dir1"));
-      fs.mkdirs(new Path("/dir2"));
-      fs.create(new Path("/f1")).close();
-      fs.create(new Path("/f2")).close();
+      webHdfs.mkdirs(new Path("/dir1"));
+      webHdfs.mkdirs(new Path("/dir2"));
+      webHdfs.create(new Path("/f1")).close();
+      webHdfs.create(new Path("/f2")).close();
 
-      FileStatus[] ls = fs.listStatus(new Path("/"));
+      FileStatus[] ls = webHdfs.listStatus(new Path("/"));
       assertEquals(4, ls.length);
+
     } finally {
+      if (webHdfs != null) {
+        webHdfs.close();
+      }
       if (cluster != null) {
         cluster.shutdown();
       }
